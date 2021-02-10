1fbc437093d37d4995d9fbbb17ba1dd4f2470549
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-10 17:51:44 -0400

    S2/claa-one: interpolation and bind

---
+++ vue-course-goal/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      message: "Finish the course and learn Vue",
+      link: "https://v3.vuejs.org/",
+    };
+  },
+  methods: {},
+});
+
+app.mount("#user-goal");
