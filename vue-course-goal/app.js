c458d341181e8ce30ab9445822c25e580b145511
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-10 18:06:26 -0400

    S2/class-one: accesing data properties in methods

--- vue-course-goal/app.js
+++ vue-course-goal/app.js
@@ -1,11 +1,21 @@
 const app = Vue.createApp({
   data() {
     return {
-      message: "Finish the course and learn Vue",
+      goalA: "Finish the course and learn Vue",
+      goalB: "Master Vue and build amazing apps!!",
       link: "https://v3.vuejs.org/",
     };
   },
-  methods: {},
+  methods: {
+    showGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.goalA;
+      } else {
+        return this.goalB;
+      }
+    },
+  },
 });
 
 app.mount("#user-goal");
