460e07c813f561e5af254659306d31102828f181
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-11 00:26:01 -0400

    assigment-03: adding the watcher

--- assigment-03/app.js
+++ assigment-03/app.js
@@ -4,6 +4,14 @@ const app = Vue.createApp({
       number: 0,
     };
   },
+  watch: {
+    result() {
+      const that = this;
+      setTimeout(() => {
+        that.number = 0;
+      }, 5000);
+    },
+  },
   computed: {
     result() {
       if (this.number < 37) {
