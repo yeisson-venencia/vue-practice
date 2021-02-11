9c691e0ebc2744db52a44808ab1c230074718b57
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-11 18:20:29 -0400

    assignment-04: adding visibility dinamicly to paragraph

--- assignment-04/app.js
+++ assignment-04/app.js
@@ -2,7 +2,18 @@ const app = Vue.createApp({
   data() {
     return {
       inputClass: "",
+      showElement: true,
     };
+  },
+  computed: {
+    paraStyle() {
+      return { visible: this.showElement, hidden: !this.showElement };
+    },
+  },
+  methods: {
+    changeVisibility() {
+      this.showElement = !this.showElement;
+    },
   },
 });
 
