8ca500a0651b2d0c56b9b29422aca9b0a7756c01
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-11 00:16:02 -0400

    assigment-03: showing the correct messages

--- assigment-03/app.js
+++ assigment-03/app.js
@@ -4,6 +4,17 @@ const app = Vue.createApp({
       number: 0,
     };
   },
+  computed: {
+    result() {
+      if (this.number < 37) {
+        return "Not there yet";
+      } else if (this.number > 37) {
+        return "Too much!";
+      } else {
+        return 37;
+      }
+    },
+  },
   methods: {
     add(num) {
       this.number += num;
