0fa9e57adc0bdb1f19d21c185e90d660c1e7409d
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-12 16:15:51 -0400

    S7/class-one: rendering the vue file in the project

---
+++ vue-first-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      persons: [
+        {
+          id: 1,
+          fullname: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: 2,
+          fullname: "Julie Jones",
+          phone: "09876 543 221",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
