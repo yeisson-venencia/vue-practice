8c9790ee9bf2c6d9371f28c872b59d6212d97451
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-13 13:20:09 -0400

    S9/class-two: rendering selected component with dynamic components

--- vue-third-project/src/App.vue
+++ vue-third-project/src/App.vue
@@ -16,8 +16,9 @@
     <br />
     <button @click="setSelectedComponent('active-goals')">Active Goals</button>
     <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
-    <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
-    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
