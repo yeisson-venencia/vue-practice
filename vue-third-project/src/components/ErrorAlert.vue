f01f3d65db15070d14dc5ef2e379f62b2853d50c
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-13 13:31:42 -0400

    S9/class-two: created generic alert component

---
+++ vue-third-project/src/components/ErrorAlert.vue
@@ -0,0 +1,16 @@
+<template>
+  <dialog>
+    <slot></slot>
+  </dialog>
+</template>
+
+<style scoped>
+dialog {
+  position: fixed;
+  top: 20vh;
+  left: 30%;
+  width: 40%;
+  background-color: white;
+  padding: 1rem;
+}
+</style>
--- vue-third-project/src/components/ManageGoals.vue
+++ vue-third-project/src/components/ManageGoals.vue
@@ -3,6 +3,19 @@
     <h2>
       Manage Goals
     </h2>
-    <input type="text" />
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (!enteredValue) {
+        alert("Error");
+      }
+    },
+  },
+};
+</script>
