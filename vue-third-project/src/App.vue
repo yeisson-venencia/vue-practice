59ffe74137a3cd58942463e75b8805cd5ff5d77b
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-13 13:10:33 -0400

    S9/class-two: added new components to the app.vue

--- vue-third-project/src/App.vue
+++ vue-third-project/src/App.vue
@@ -13,6 +13,9 @@
         <p>{{ slotProps["another"] }}</p>
       </template>
     </course-goals>
+    <br />
+    <active-goals></active-goals>
+    <manage-goals></manage-goals>
   </div>
 </template>
 
@@ -21,9 +24,18 @@ import TheHeader from "./components/TheHeader";
 import BadgeList from "./components/BadgeList";
 import UserInfo from "./components/UserInfo";
 import CourseGoals from "./components/CourseGoals";
+import ManageGoals from "./components/ManageGoals";
+import ActiveGoals from "./components/ActiveGoals";
 
 export default {
-  components: { TheHeader, BadgeList, UserInfo, CourseGoals },
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo,
+    CourseGoals,
+    ManageGoals,
+    ActiveGoals,
+  },
   data() {
     return {
       activeUser: {
